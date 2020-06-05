862ea635e7e59a6e32846913651dff366d6d0499
Nivall <38981440+k-kotov@users.noreply.github.com>
2020-06-05 15:55:38 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,4 @@
 
-variable "default_network_acl_egress" {
-  description = "List of maps of egress rules to set on the Default Network ACL"
-  type        = list(map(string))
-
-  default = [
-    {
-      rule_no    = 100
-      action     = "allow"
-      from_port  = 0
-      to_port    = 0
-      protocol   = "-1"
-      cidr_block = "10.0.0.0/16"
-    },
-    {
-      rule_no         = 101
-      action          = "allow"
-      from_port       = 0
-      to_port         = 0
-      protocol        = "-1"
-      ipv6_cidr_block = "::/0"
-    },
-  ]
-}
-
 variable "public_inbound_acl_rules" {
   description = "Public subnets inbound network ACLs"
   type        = list(map(string))
