7bc7eae6022d71c011a876ac4c53f9bc6ee5ac7a
Nivall <38981440+k-kotov@users.noreply.github.com>
2020-06-05 15:59:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ locals {
 resource "aws_vpc" "this" {
   count = var.create_vpc ? 1 : 0
 
-  cidr_block                       = var.cidr
+  cidr_block                       = "10.0.0.0/16"
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
   enable_dns_support               = var.enable_dns_support
